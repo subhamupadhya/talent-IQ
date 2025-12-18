2cf452e0c7373fcbbcee494508d8f4e67bb57c29
Subham Kumar <subhamupadhya53100@gmail.com>
2025-12-19 02:33:59 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { ENV } from "./lib/env.js";
+
+
+
+const app = express();
+
+console.log(ENV.PORT);
+console.log(ENV.DB_URL);
+
+
+app.get("/health", (req, res) => {
+    res.status(200).json({msg:"api is up and running"})
+});
+
+app.listen(ENV.PORT, () => console.log("server is running on port:", ENV.PORT));
