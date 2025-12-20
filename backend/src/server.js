a95012246bf311526e87d187b302957ae462eefd
Subham Kumar <subhamupadhya53100@gmail.com>
2025-12-20 17:01:57 +0530

    Database & Tools setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path"
 
 import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 
 
 
@@ -27,4 +28,14 @@ if(ENV.NODE_ENV === "production") {
     });
 }
 
-app.listen(ENV.PORT, () => console.log("server is running on port:", ENV.PORT));
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, () => console.log(" Sever is running on port:", ENV.PORT));
+    } catch (error) {
+        console.error(" Error starting the server", error);
+    }
+};
+
+startServer();
