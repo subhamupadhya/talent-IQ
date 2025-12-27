ad31e525623475daee18d6d5f4fc3b3cfc14a2e5
Subham Kumar <subhamupadhya53100@gmail.com>
2025-12-28 02:12:15 +0530

    minor typo fixes

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -7,12 +7,12 @@ export const protectRoute = [
         try{
             const clerkId = req.auth().userId;
 
-            if(!clerkId) return res.status(401).json({ msg: "Unauthorized - invalid token "});
+            if(!clerkId) return res.status(401).json({ message: "Unauthorized - invalid token "});
 
             // finally user in db by clerk id
             const user = await User.findOne({ clerkId });
             
-            if(!user) return res.stauts(404).json({ msg: "User not found "});
+            if(!user) return res.stauts(404).json({ message: "User not found "});
 
             // attact user to req
             req.user = user;
